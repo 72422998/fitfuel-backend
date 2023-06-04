8766da5a4b2df8a2974b093d0799693577c4cda2
72422998 <81695790+72422998@users.noreply.github.com>
2023-06-04 18:09:53 -0500

    feat: Se agrego un controlador
    Se agrego el controlador de nutricionista

---
+++ src/main/java/pe/com/fitfuel/models/Nutricionista.java
@@ -0,0 +1,88 @@
+package pe.com.fitfuel.models;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Nutricionista {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private Integer dni;
+    private String profesion;
+    private String descripcion;
+    private String foto;
+    private String video;
+    private boolean estado;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Integer getDni() {
+        return dni;
+    }
+
+    public void setDni(Integer dni) {
+        this.dni = dni;
+    }
+
+    public String getProfesion() {
+        return profesion;
+    }
+
+    public void setProfesion(String profesion) {
+        this.profesion = profesion;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public String getFoto() {
+        return foto;
+    }
+
+    public void setFoto(String foto) {
+        this.foto = foto;
+    }
+
+    public String getVideo() {
+        return video;
+    }
+
+    public void setVideo(String video) {
+        this.video = video;
+    }
+
+    public boolean isEstado() {
+        return estado;
+    }
+
+    public void setEstado(boolean estado) {
+        this.estado = estado;
+    }
+
+    public Nutricionista(Integer id, Integer dni, String profesion, String descripcion, String foto, String video, boolean estado) {
+        this.id = id;
+        this.dni = dni;
+        this.profesion = profesion;
+        this.descripcion = descripcion;
+        this.foto = foto;
+        this.video = video;
+        this.estado = estado;
+    }
+
+    public Nutricionista() {
+    }
+}

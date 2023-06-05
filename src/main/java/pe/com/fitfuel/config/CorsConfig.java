97e035558c8e7996d70ea683368ff6971b7e9cc3
72422998 <81695790+72422998@users.noreply.github.com>
2023-06-04 20:29:08 -0500

    feat: Creacion de Cors
    Se creo cors y se implementaron los permisos para frontend

---
+++ src/main/java/pe/com/fitfuel/config/CorsConfig.java
@@ -0,0 +1,16 @@
+package pe.com.fitfuel.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry){
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:3000","http://127.0.0.1:5173")
+                .allowedOrigins("GET","POST","PUT","DELETE")
+                .allowedOrigins("*");
+    }
+}
